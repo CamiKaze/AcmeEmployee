0a00ac4c070997877d5361dc2eeac37f3a423e19
iqb_cameron <csmith@iqbusiness.net>
2020-02-02 19:05:01 +0200

    Added Tests

---
+++ EmployeeClient/Utility.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace EmployeeClient
+{// Single responsibility - The class is responsible for creating an email address only
+    public partial class EmailCreator
+    {// There are no email addresses in the JSON file.
+     // I am creating a valid email address format with this class
+        public static string Create(string fname, string lname)
+            => Create(fname, lname, "@acme.com");
+
+        public static string Create(string fname, string lname, string domain)
+        {// I am replacing all the invalid characters with nothing
+            string email = fname.Trim() + lname.Trim() + domain.Trim();
+            string emailResult = Regex.Replace(email, @"\s", "");
+            return emailResult;
+        }
+    }
+}
---
+++ EmployeeClientTests/UtilityTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace EmployeeClient.Tests
+{
+    [TestClass()]
+    public class UtilityTests
+    {
+        [TestMethod()]
+        public void IsValidEmailTest()
+        {
+            string email = string.Empty;
+
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Error code: " + ex.Message); //  return false;
+            }
+            Assert.Fail();
+        }
+    }
+}
